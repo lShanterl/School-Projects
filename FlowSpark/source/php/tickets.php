639744b2d2907224dc875b54fb8bd9b6e678d92e
Bartosz Starzyk <bratmat.yt@gmail.com>
2023-03-31 14:58:51 +0200

    tickets

--- FlowSpark/source/php/tickets.php
+++ FlowSpark/source/php/tickets.php
@@ -23,10 +23,7 @@
             <?php if($cookie){ ?>
                 <a href="./watchlist.php">Your Watchlist</a>
             <?php } ?>
-        </div>
-        <div class="mid">
-            <!-- Mobile navbar part -->
-        </div>
+        </div>    
         <div class="right">
             <div>
                 <input type="text" id="search" placeholder="Search..." name='search'>
@@ -37,7 +34,7 @@
                 <?php if($admin == 1){ ?>
                         <a href="./adminpanel.php"><button class='login'>Admin</button></a>
                 <?php } ?>
-                <a href="./settings.php"><img class='avatar' src=<?php echo GetIcon()?> alt=""></a>
+                <a href="./settings.php"><img class='avatar' draggable="false" src=<?php echo GetIcon()?> alt=""></a>
             <?php }else{ ?>
                 <a href="./login.php"><button class='login'>Log in</button></a>
                 <a href="./signup.php"><button class='login'>Sign up</button></a>
@@ -47,23 +44,28 @@
     </nav>
     <div class="container">
             <div class="movie">
-
             </div>
-            <div class="seats">
-                <?php
-
-                    $sql = "SELECT * FROM seats where cinema_hall_id = 1";
-
-                    $result = mysqli_query($conn, $sql);
-                    while($row = mysqli_fetch_assoc($result)){
-                        if($row['row_number'] == 4){
-                            var_dump($row['row_number']);
-                        }
-                        ?>
-                        <button class="seat" style='<?php echo $row['row_number']?> / <?php echo $row['seat_number'] ?> / <?php echo $row['row_number'] ?> / <?php echo $row['seat_number'] ?>'><?php echo $row['seat_number'] ?></button>
-                    <?php } ?>
-
-            </div>
+            <?php 
+                $sql = "SELECT * FROM cinema_hall where id = 1";
+                $result = mysqli_query($conn, $sql);
+                $row = mysqli_fetch_assoc($result);
+            
+            ?>
+            <div class="seats-wrapper">
+    <div class="seats" style='grid-template-columns: repeat(<?php echo $row['seats_per_row']; ?>, 25px); grid-template-rows: repeat(<?php echo $row['total_rows']; ?>, 25px);'>
+        <?php
+            $sql = "SELECT * FROM seats where cinema_hall_id = 1";
+            $result = mysqli_query($conn, $sql);
+            while($row = mysqli_fetch_assoc($result)){
+        ?>
+        <span style='<?php echo $row['row_number']?> / <?php echo $row['seat_number'] ?> / <?php echo $row['row_number'] ?> / <?php echo $row['seat_number'] ?>'>
+            <input type="checkbox" id="seat-<?php echo $row['id']?>" name="seats[]" value="<?php echo $row['row_number'];?>-<?php echo $row['seat_number'];?>">
+            <label for="seat-<?php echo $row['id']; ?>" data-seat-number="<?php echo $row['seat_number'];?>"></label>
+        </span>
+        <?php } ?>
+    </div>
+</div>
+            
     </div>  
 </body>
 </html>
