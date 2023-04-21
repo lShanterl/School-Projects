d42705def8fba2dccad8b963d5f63526bbbb3749
Bartosz Starzyk <bratmat.yt@gmail.com>
2023-04-21 14:08:44 +0200

    password

--- FlowSpark/source/php/edit_user.php
+++ FlowSpark/source/php/edit_user.php
@@ -41,7 +41,8 @@
             header("Location: ./adminpanel-movies.php?error=avatar");
         }
     }
-    $sql = "UPDATE users SET name = '$name', surname = '$surname', email = '$email', isAdmin = '$isAdmin'".(isset($avatar) ? ", image_path = $avatar" : ''). (!$password == null ? ",password = $password" : '')  ." WHERE id = $id";
+    $sql = "UPDATE users SET name = '$name', surname = '$surname', email = '$email', isAdmin = '$isAdmin'".(isset($avatar) ? ", image_path = '$avatar'" : ''). (!$password == null ? ", password = '$password'" : '')  ." WHERE id = $id";
+
 
     if(mysqli_query($conn, $sql)){
         header("Location: ./adminpanel.php?success=edit");
